ecf6dc5f350ca5da5757dd395bdcddb9dc047048
Matt <mvaden08@gmail.com>
2016-03-24 19:10:15 -0400

    atm lab with Josh.

--- atm.js
+++ atm.js
@@ -1,1 +1,27 @@
-// your code goes here!
+var deposit1 = document.querySelector ("#checkingDepositButton")
+var withdraw1 = document.querySelector ("#checkingWithdrawalButton")
+var deposit2 = document.querySelector ("#savingsDepositButton")
+var withdraw2 = document.querySelector ("#savingsWithdrawalButton")
+
+function handleClickEvent (){
+
+  console.log("I was clicked");
+  console.log(this);
+
+}
+// This is the eventListener.
+button.addEventListener("click", handleClickEvent );
+
+
+
+//This is the eventListener.
+deposit1.addEventListener("click", handleClickEvent );
+withdraw1.addEventListener("click", handleClickEvent );
+deposit2.addEventListener("click", handleClickEvent );
+withdraw2.addEventListener("click", handleClickEvent );
+input.addEventListener("onClick" = function());
+
+document.getElementById("#checkingDepositButton").addEventListener("click", something);
+document.getElementById("#checkingWithdrawalButton").addEventLister("click", something );
+document.getElementById("checkingDepositButton").addEventListener("click", something ) ;
+document.getElementById("checkingDepositButton").addEventListener("click", something ) ;
